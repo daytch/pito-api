0d60781481630e43f4d3a6d8eb67cfd94ef8d81b
daytch <van.daytch@gmail.com>
2020-10-23 17:22:34 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080,
+    bodyParser = require('body-parser'),
+    cors = require("cors");
+
+var corsOptions = { origin: [ "http://5.189.134.84:3000"] }; // "https://pito.vercel.app/",
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./routes');
+routes(app);
+
+app.listen(port);
+console.log('RESTful API server started on: ' + port);
