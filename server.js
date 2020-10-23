5fc696e22eff92c94ffea97b0be97c44dc61ab11
daytch <van.daytch@gmail.com>
2020-10-23 19:17:41 +0700

    fixing

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     cors = require("cors");
 
-var corsOptions = { origin: ["https://pito.vercel.app/"] };
-app.use(cors(corsOptions));
+// var corsOptions = { origin: ["https://pito.vercel.app/"] };
+// app.use(cors(corsOptions));
+
+var allowedOrigins = ["https://pito.vercel.app"];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
